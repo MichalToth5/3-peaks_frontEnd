7ba3827fc5642858b91fec52a788f1668673fccb
AnetaSzolikova <anetaszolikova6@gmail.com>
2022-04-02 18:03:13 +0200

    Vytvorenie formulára, zoznamu a stránky pre vakcínu

--- src/app/vaccine/vaccine-page/vaccine-page.component.ts
+++ src/app/vaccine/vaccine-page/vaccine-page.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Vaccine} from "../../models/vaccine.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-vaccine-page',
   templateUrl: './vaccine-page.component.html',
   styleUrls: ['./vaccine-page.component.css']
 })
-export class VaccinePageComponent implements OnInit {
+export class VaccinePageComponent {
 
-  constructor() { }
+  vaccines: Vaccine[] = [];
 
-  ngOnInit(): void {
+  constructor(private router: Router) { }
+
+  goBack(): void {
+    this.router.navigate(['']);
+  }
+
+  addVaccine(vaccine: Vaccine): void {
+
   }
 
 }
