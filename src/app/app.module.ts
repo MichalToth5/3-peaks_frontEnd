a09c554b973133cf338a1ce140e906992dd40eb4
AnetaSzolikova <anetaszolikova6@gmail.com>
2022-05-21 19:11:28 +0200

    update app.module.ts bcs keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PatientFormComponent } from './patient/patient-form/patient-form.component';
 import { PatientListComponent } from './patient/patient-list/patient-list.component';
-
 import { VaccineFormComponent } from './vaccine/vaccine-form/vaccine-form.component';
 import { VaccineListComponent } from './vaccine/vaccine-list/vaccine-list.component';
-
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppRoutingModule} from "./app-routing.module";
-
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {Ng2SearchPipeModule} from "ng2-search-filter";
 import { PatientShotFormComponent } from './patient/patient-shot-form/patient-shot-form.component';
+import {NullValidationHandler, OAuthModule, OAuthService} from "angular-oauth2-oidc";
+import {authCodeFlowConfig} from "./AuthConfig";
+import {RouterModule} from "@angular/router";
+import {AuthInterceptor} from "./AuthInterceptor";
+
+function init_app(oauthService: OAuthService) {
+  return () => configureWithNewConfigApi(oauthService);
+}
+function configureWithNewConfigApi(oauthService: OAuthService) {
+  oauthService.configure(authCodeFlowConfig);
+  oauthService.tokenValidationHandler = new NullValidationHandler();
+  oauthService.setupAutomaticSilentRefresh();
+  oauthService.events.subscribe(e => { });
+  return oauthService.loadDiscoveryDocumentAndTryLogin();
+}
 
 @NgModule({
   declarations: [
@@ -25,9 +36,7 @@ import { PatientShotFormComponent } from './patient/patient-shot-form/patient-sh
     VaccineFormComponent,
     VaccineListComponent,
     AdminLayoutComponent,
-    PatientShotFormComponent,
-
-
+    PatientShotFormComponent
   ],
   imports: [
     BrowserModule,
@@ -37,9 +46,25 @@ import { PatientShotFormComponent } from './patient/patient-shot-form/patient-sh
     NgbModule,
     Ng2SearchPipeModule,
     FormsModule,
-    //NgbModal
+    OAuthModule.forRoot(),
+    HttpClientModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: init_app,
+      deps: [
+        OAuthService
+      ],
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
