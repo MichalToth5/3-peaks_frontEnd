cc63b00ced6ad2c4a9aff38d9c068465307e5fbc
MichalToth5 <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>
2022-05-22 11:36:14 +0200

    Redirect after login

--- src/app/AuthConfig.ts
+++ src/app/AuthConfig.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'http://localhost:9900/auth/realms/your-project',
-  redirectUri: 'http://localhost:4200',
+  redirectUri: 'http://localhost:4200/admin/patient',
   clientId: 'fe-app',
   scope: 'openid profile email',
   requireHttps: false,
