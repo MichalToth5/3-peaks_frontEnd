184851aaa268fcfa440dd27a4b93bbf070e8a80a
aulinka <laura.mikusova27@gmail.com>
2022-05-23 23:38:27 +0200

    login/logout + footer and nav bar (ng bootstrap + bs)

--- src/app/admin-layout/admin-layout.component.ts
+++ src/app/admin-layout/admin-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NcziService} from "../services/nczi.service";
 import {Nczi} from "../models/nczi.model";
+import {OAuthService} from "angular-oauth2-oidc";
 
 
 @Component({
@@ -13,7 +14,7 @@ export class AdminLayoutComponent implements OnInit {
   vaccinations: Nczi;
   hospitalPatients: Nczi;
   positivePatients: Nczi
-  constructor(private service:NcziService) { }
+  constructor(private service:NcziService, private oauthservice: OAuthService) { }
 
   ngOnInit(): void {
     this.service.getVaccinations().subscribe(data =>{
@@ -25,5 +26,12 @@ export class AdminLayoutComponent implements OnInit {
     this.service.getPositives().subscribe(data =>{
       this.positivePatients = data;
     })
+  }
+  odhlasenie() {
+    this.oauthservice.logOut();
+    this.oauthservice.loadDiscoveryDocumentAndLogin();
+  }
+  prihlasenie() {
+    this.oauthservice.loadDiscoveryDocumentAndLogin();
   }
 }
