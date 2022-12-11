6d794a168114055a4483e0fbfb17a710fa701d42
aulinka <laura.mikusova27@gmail.com>
2022-12-11 21:48:05 +0100

    rabbitmq

--- src/app/AuthConfig.ts
+++ src/app/AuthConfig.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: 'http://localhost:8080/auth/realms/your-project',
+  issuer: 'http://keycloak:8080/auth/realms/your-project',
   redirectUri: 'http://localhost/admin/patient',
   clientId: 'fe-app',
   scope: 'openid profile email',
